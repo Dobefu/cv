75ef6a42e18d93618abc65e4ed79b2a2399abf9f
Connor van Spronssen <info@dobefu.com>
2025-01-04 13:36:49 +0100

    feat: Add some ESLint plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,44 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y'
+import pluginReact from 'eslint-plugin-react'
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: import.meta.dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  }),
+  {
+    ...pluginJsxA11y.flatConfigs.strict,
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      'react/boolean-prop-naming': ['warn'],
+      'react/forward-ref-uses-ref': ['warn'],
+      'react/hook-use-state': ['warn'],
+      'react/iframe-missing-sandbox': ['warn'],
+      'react/jsx-boolean-value': ['warn', 'never'],
+      'react/jsx-fragments': ['warn'],
+      'react/jsx-handler-names': ['warn'],
+      'react/jsx-no-bind': ['error'],
+      'react/jsx-no-leaked-render': ['error'],
+      'react/jsx-no-useless-fragment': ['warn', { allowExpressions: true }],
+      'react/jsx-props-no-spread-multi': ['warn'],
+      'react/sort-default-props': ['warn'],
+      'react/jsx-sort-props': ['warn'],
+      'react/no-array-index-key': ['error'],
+      'react/no-invalid-html-attribute': ['warn'],
+      'react/no-this-in-sfc': ['warn'],
+      'react/no-unstable-nested-components': ['error'],
+      'react/no-unused-state': ['warn'],
+      'react/no-will-update-set-state': ['warn'],
+      'react/prefer-read-only-props': ['warn'],
+      'react/prefer-stateless-function': ['error'],
+      'react/require-default-props': ['error'],
+      'react/style-prop-object': ['warn'],
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
