ca6efd876cf4d995515f5d2d6df95f0261e573d0
Connor van Spronssen <info@dobefu.com>
2025-01-04 19:49:52 +0100

    feat: Add a footer

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,82 @@
+import { Icon } from '@iconify/react'
+import getConfig from 'next/config'
+import Link from 'next/link'
+
+export default function Footer() {
+  const { publicRuntimeConfig } = getConfig()
+  const appName = publicRuntimeConfig.appName || ''
+
+  const footerLinks = [
+    {
+      title: 'Sitemap',
+      to: '/sitemap.xml',
+    },
+  ]
+
+  const socialLinks = [
+    {
+      title: 'LinkedIn',
+      to: 'https://www.linkedin.com/in/connor-van-spronssen/',
+      icon: 'bi:linkedin',
+    },
+    {
+      title: 'Github',
+      to: 'https://github.com/Dobefu',
+      icon: 'bi:github',
+    },
+    {
+      title: 'Gitlab',
+      to: 'https://gitlab.com/Dobefu',
+      icon: 'bi:gitlab',
+    },
+    {
+      title: 'Drupal',
+      to: 'https://drupal.org/u/Dobefu',
+      icon: 'simple-icons:drupal',
+    },
+  ]
+
+  return (
+    <footer className="w-full bg-zinc-100 px-4 py-10 shadow-[0_-4px_6px_-1px_rgb(0,0,0,.1)] dark:bg-zinc-950">
+      <div className="mx-auto grid max-w-5xl grid-cols-1 items-center gap-5 text-center md:grid-cols-3">
+        <div className="md:text-start">
+          <Link
+            aria-label={appName}
+            className="flex-none text-xl font-semibold text-zinc-800 dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            href="/"
+          >
+            {appName}
+          </Link>
+        </div>
+
+        <div className="text-center">
+          {footerLinks.map((footerLink) => (
+            <Link
+              aria-label={footerLink.title}
+              className="gap-x-2 text-sm text-gray-500 transition-colors hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              href={footerLink.to}
+              key={footerLink.to}
+            >
+              {footerLink.title}
+            </Link>
+          ))}
+        </div>
+
+        <div className="space-x-2 md:text-end">
+          {socialLinks.map((socialLink) => (
+            <Link
+              aria-label={socialLink.title}
+              className="inline-flex size-8 items-center justify-center gap-x-2 rounded-full border border-transparent text-sm font-semibold text-gray-500 transition-colors hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              href={socialLink.to}
+              key={socialLink.to}
+              target="_blank"
+              title={socialLink.title}
+            >
+              <Icon className="h-5 w-5" icon={socialLink.icon} ssr />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </footer>
+  )
+}
--- src/components/layout/root-layout.tsx
+++ src/components/layout/root-layout.tsx
@@ -1,6 +1,7 @@
 import getLocales from '@/utils/get-locales'
 import getTranslations from '@/utils/get-translations'
 import { Geist } from 'next/font/google'
+import Footer from './footer'
 import './globals.css'
 import Header from './header'
 import Providers from './providers'
@@ -26,7 +27,7 @@ export default async function RootLayout({
   return (
     <html className="h-full" lang={locale.code}>
       <body
-        className={`${geistSans.variable} flex min-h-full flex-col gap-4 bg-zinc-200 font-sans text-zinc-900 antialiased dark:bg-zinc-900 dark:text-white contrast-more:dark:bg-black`}
+        className={`${geistSans.variable} flex min-h-full flex-col bg-zinc-200 font-sans text-zinc-900 antialiased dark:bg-zinc-900 dark:text-white contrast-more:dark:bg-black`}
       >
         <Providers locale={locale} translations={translations}>
           <div className="flex flex-1 flex-col justify-between">
@@ -38,6 +39,8 @@ export default async function RootLayout({
             >
               {children}
             </main>
+
+            <Footer />
           </div>
         </Providers>
       </body>
