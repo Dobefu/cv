25b40cdff0517b6d36ce9c78ed548339158083e7
Connor van Spronssen <info@dobefu.com>
2025-01-04 22:18:52 +0100

    test: Add tests to the providers

---
+++ src/components/layout/providers.test.tsx
@@ -0,0 +1,21 @@
+import LocaleLink from '@/components/utils/locale-link'
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it } from 'vitest'
+import Providers from './providers'
+
+describe('providers', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('Renders', () => {
+    render(
+      <Providers locale={{ code: 'en', name: 'English' }} translations={{}}>
+        <LocaleLink href="/">Test</LocaleLink>
+      </Providers>,
+    )
+
+    expect(screen.getByRole('link')).toBeDefined()
+    expect(screen.getByRole('link').getAttribute('href')).toBe('/en')
+  })
+})
