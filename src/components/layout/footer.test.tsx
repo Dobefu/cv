2ed88588499177b23f0fc321edd46314c4e7fc75
Connor van Spronssen <info@dobefu.com>
2025-01-04 22:26:56 +0100

    test: Add tests to the Footer component

---
+++ src/components/layout/footer.test.tsx
@@ -0,0 +1,16 @@
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import Footer from './footer'
+
+describe('Footer', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+    cleanup()
+  })
+
+  it('Renders', () => {
+    render(<Footer appName="Testing" />)
+
+    expect(screen.getByRole('contentinfo')).toBeDefined()
+  })
+})
