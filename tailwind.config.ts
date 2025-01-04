695eb6a06ff3fdc42a578f1534f13eb91e778bf9
Connor van Spronssen <info@dobefu.com>
2025-01-04 15:45:24 +0100

    feat: Remove the theme extensions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,7 @@ export default {
     './src/content/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config
