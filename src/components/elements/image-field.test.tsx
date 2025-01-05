8b05ecec9110659d4f736597839850b2c5a00794
Connor van Spronssen <info@dobefu.com>
2025-01-05 15:55:56 +0100

    test: Add tests to the ImageField component

---
+++ src/components/elements/image-field.test.tsx
@@ -0,0 +1,76 @@
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import ImageField from './image-field'
+
+describe('ImageField', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+    cleanup()
+  })
+
+  it('Renders', () => {
+    render(
+      <ImageField
+        img={{
+          src: '/',
+          alt: '',
+          height: 10,
+          width: 10,
+        }}
+        label="Test label"
+        link="/"
+        subtext="Test subtext"
+        text="Test text"
+      />,
+    )
+
+    expect(screen.getByText('Test label')).toBeDefined()
+    expect(screen.getByText('Test text')).toBeDefined()
+    expect(screen.getByText('Test subtext')).toBeDefined()
+  })
+
+  it('Renders without an image', () => {
+    render(
+      <ImageField
+        label="Test label"
+        link="/"
+        subtext="Test subtext"
+        text="Test text"
+      />,
+    )
+
+    expect(screen.getByText('Test label')).toBeDefined()
+    expect(screen.getByText('Test text')).toBeDefined()
+    expect(screen.getByText('Test subtext')).toBeDefined()
+  })
+
+  it('Renders without a link', () => {
+    render(
+      <ImageField
+        img={{
+          src: '/',
+          alt: '',
+          height: 10,
+          width: 10,
+        }}
+        label="Test label"
+        subtext="Test subtext"
+        text="Test text"
+      />,
+    )
+
+    expect(screen.getByText('Test label')).toBeDefined()
+    expect(screen.getByText('Test text')).toBeDefined()
+    expect(screen.getByText('Test subtext')).toBeDefined()
+  })
+
+  it('Renders without any optional fields', () => {
+    render(
+      <ImageField label="Test label" subtext="Test subtext" text="Test text" />,
+    )
+
+    expect(screen.getByText('Test label')).toBeDefined()
+    expect(screen.getByText('Test text')).toBeDefined()
+    expect(screen.getByText('Test subtext')).toBeDefined()
+  })
+})
