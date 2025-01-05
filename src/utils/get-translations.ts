70ae201b1c3d2c8a4566feefd3a361e707a94dd3
Connor van Spronssen <info@dobefu.com>
2025-01-05 13:42:07 +0100

    feat: Add some translations for the language switcher

--- src/utils/get-translations.ts
+++ src/utils/get-translations.ts
@@ -5,9 +5,15 @@ export default function getTranslations(
     {
       en: {
         'layout.skip_to_main': 'Skip to main content',
+        'locale_switcher.label': 'Choose a language',
+        'languages.en': 'English',
+        'languages.nl': 'Dutch',
       },
       nl: {
         'layout.skip_to_main': 'Naar hoofdinhoud gaan',
+        'locale_switcher.label': 'Kies een taal',
+        'languages.en': 'Engels',
+        'languages.nl': 'Nederlands',
       },
     }[locale] || {}
   )
