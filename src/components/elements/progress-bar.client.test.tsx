60aa0ebc33ae71189f50dea0e2f6c0fd5309262f
Connor van Spronssen <info@dobefu.com>
2025-01-05 17:07:35 +0100

    test: Add a test to the ProgressBar component

---
+++ src/components/elements/progress-bar.client.test.tsx
@@ -0,0 +1,16 @@
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import ProgressBar from './progress-bar.client'
+
+describe('ProgressBar', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+    cleanup()
+  })
+
+  it('Renders', () => {
+    render(<ProgressBar label="Testing" percentage={40} />)
+
+    expect(screen.getByText('Testing')).toBeDefined()
+  })
+})
--- vitestSetup.ts
+++ vitestSetup.ts
@@ -30,3 +30,12 @@ vi.mock('next/navigation', async () => {
     },
   }
 })
+
+const IntersectionObserverMock = vi.fn(() => ({
+  disconnect: vi.fn(),
+  observe: vi.fn(),
+  takeRecords: vi.fn(),
+  unobserve: vi.fn(),
+}))
+
+vi.stubGlobal('IntersectionObserver', IntersectionObserverMock)
