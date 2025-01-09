87fa46a5df74043b321a900fb9cf18e76d2a94bc
Connor van Spronssen <info@dobefu.com>
2025-01-09 17:26:52 +0100

    feat: Style the 404 page

--- src/app/nl/not-found.tsx
+++ src/app/nl/not-found.tsx
@@ -1,9 +1,11 @@
 export default function NotFound() {
   return (
-    <>
-      <h1>Pagina niet gevonden</h1>
+    <div className="relative px-4">
+      <div className="container mx-auto max-w-5xl overflow-hidden rounded-2xl bg-white/80 p-12 shadow-md backdrop-blur-lg contrast-more:bg-white dark:bg-neutral-800/90 contrast-more:dark:bg-neutral-900 print:bg-white">
+        <h2>Page not found</h2>
 
-      <p>De opgevraagde pagina kon niet worden gevonden.</p>
-    </>
+        <p>The requested page could not be found.</p>
+      </div>
+    </div>
   )
 }
