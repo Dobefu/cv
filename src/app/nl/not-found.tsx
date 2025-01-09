3ca832ed0ffcd0ea7bd70711b2c3273125cbbcfa
Connor van Spronssen <info@dobefu.com>
2025-01-09 17:37:33 +0100

    fix: Re-add the Dutch translation on the 404 page

--- src/app/nl/not-found.tsx
+++ src/app/nl/not-found.tsx
@@ -2,9 +2,9 @@ export default function NotFound() {
   return (
     <div className="relative px-4">
       <div className="container mx-auto max-w-5xl overflow-hidden rounded-2xl bg-white/80 p-12 shadow-md backdrop-blur-lg contrast-more:bg-white dark:bg-neutral-800/90 contrast-more:dark:bg-neutral-900 print:bg-white">
-        <h2>Page not found</h2>
+        <h2>Pagina niet gevonden</h2>
 
-        <p>The requested page could not be found.</p>
+        <p>De opgevraagde pagina kon niet worden gevonden.</p>
       </div>
     </div>
   )
