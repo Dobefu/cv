6f64c64f1637a1158d0861f509178d403a6ecb81
Connor van Spronssen <info@dobefu.com>
2025-01-30 14:53:27 +0100

    fix: Use the ContentContainer for several pages instead of duplicating classes

--- src/app/en/projects/page.tsx
+++ src/app/en/projects/page.tsx
@@ -1,11 +1,12 @@
+import ContentContainer from '@/components/layout/content-container'
 import Client from './page.client'
 
 export default async function Projects() {
   return (
     <div className="relative px-4">
-      <div className="container mx-auto max-w-5xl overflow-hidden rounded-2xl bg-white/80 p-12 shadow-md backdrop-blur-lg contrast-more:bg-white dark:bg-neutral-800/90 contrast-more:dark:bg-neutral-900 print:bg-white">
+      <ContentContainer>
         <Client />
-      </div>
+      </ContentContainer>
     </div>
   )
 }
--- src/app/nl/not-found.tsx
+++ src/app/nl/not-found.tsx
@@ -1,11 +1,13 @@
+import ContentContainer from '@/components/layout/content-container'
+
 export default function NotFound() {
   return (
     <div className="relative px-4">
-      <div className="container mx-auto max-w-5xl overflow-hidden rounded-2xl bg-white/80 p-12 shadow-md backdrop-blur-lg contrast-more:bg-white dark:bg-neutral-800/90 contrast-more:dark:bg-neutral-900 print:bg-white">
+      <ContentContainer>
         <h2>Pagina niet gevonden</h2>
 
         <p>De opgevraagde pagina kon niet worden gevonden.</p>
-      </div>
+      </ContentContainer>
     </div>
   )
 }
