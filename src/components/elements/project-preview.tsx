3287f3d1d809456217a29fd61b959fbff9b93265
Connor van Spronssen <info@dobefu.com>
2025-01-31 15:20:46 +0100

    feat: Move tags to a separate component

--- src/components/elements/project-preview.tsx
+++ src/components/elements/project-preview.tsx
@@ -3,6 +3,7 @@ import getTag from '@/utils/get-tag'
 import { Icon } from '@iconify/react'
 import Image from 'next/image'
 import LocaleLink from '../utils/locale-link.client'
+import IconTag from './tag'
 
 type Props = Readonly<{
   label: string
@@ -41,23 +42,9 @@ export default function ProjectPreview({
         <p>{subtext}</p>
 
         <div className="flex gap-2">
-          {tags.map((tagId) => {
-            const tag = getTag(tagId)
-
-            return (
-              <span
-                className="flex items-center gap-1 rounded-full border border-white bg-zinc-100 px-3 py-1 text-sm shadow dark:border-zinc-700 dark:bg-zinc-900"
-                key={tagId}
-              >
-                <Icon
-                  className={cn('h-4 w-4', tag.classes)}
-                  icon={tag.icon}
-                  ssr
-                />
-                {tag.title}
-              </span>
-            )
-          })}
+          {tags.map((tagId) => (
+            <IconTag key={tagId} tagId={tagId} />
+          ))}
         </div>
       </div>
     </LocaleLink>
