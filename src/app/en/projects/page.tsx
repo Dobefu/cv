e8557bad8937224be7e919b55d0b9da16676d5ee
Connor van Spronssen <info@dobefu.com>
2025-01-31 13:56:18 +0100

    feat: Add some intro texts to the project overview page

--- src/app/en/projects/page.tsx
+++ src/app/en/projects/page.tsx
@@ -1,6 +1,12 @@
+import ImageField from '@/components/elements/image-field'
 import ContentContainer from '@/components/layout/content-container'
 import getProjects from '@/utils/get-projects'
-import Client from './page.client'
+
+export const metadata = {
+  title: 'Projects',
+  description:
+    'As a passionate full-stack web engineer, I have honed my skills. Explore my projects to see how I transform ideas into impactful web experiences.',
+}
 
 export default function Projects() {
   const projects = getProjects()
@@ -8,7 +14,36 @@ export default function Projects() {
   return (
     <div className="relative px-4">
       <ContentContainer>
-        <Client projects={projects} />
+        <h2>Projects</h2>
+
+        <div className="flex flex-col gap-2 pb-8 text-gray-700 dark:text-gray-200">
+          <p>
+            As a passionate full-stack web engineer, I have honed my skills
+            across a wide range of programming languages and frameworks. Over
+            the years, I have worked on diverse professional and personal
+            projects that reflect my ability to solve complex problems and
+            deliver high-quality solutions.
+          </p>
+
+          <p>
+            Explore the projects below to get a better sense of how I apply my
+            expertise to real-world challenges.
+          </p>
+        </div>
+
+        {projects?.map((project) => (
+          <ImageField
+            img={{
+              src: `/img/projects/${project.image}`,
+              alt: '',
+            }}
+            key={project.title}
+            label={project.title}
+            link={`/en/projects/${project.path}`}
+            subtext=""
+            text=""
+          />
+        ))}
       </ContentContainer>
     </div>
   )
--- src/app/nl/projects/page.tsx
+++ src/app/nl/projects/page.tsx
@@ -1,6 +1,12 @@
+import ImageField from '@/components/elements/image-field'
 import ContentContainer from '@/components/layout/content-container'
 import getProjects from '@/utils/get-projects'
-import Client from './page.client'
+
+export const metadata = {
+  title: 'Projecten',
+  description:
+    'Als gepassioneerd full-stack web engineer heb ik mijn vaardigheden verbeterd. Ontdek mijn projecten om te zien hoe ik ideeën omzet in krachtige webervaringen.',
+}
 
 export default function Projects() {
   const projects = getProjects()
@@ -8,7 +14,37 @@ export default function Projects() {
   return (
     <div className="relative px-4">
       <ContentContainer>
-        <Client projects={projects} />
+        <h2>Projecten</h2>
+
+        <div className="flex flex-col gap-2 pb-8 text-gray-700 dark:text-gray-200">
+          <p>
+            Als een gepassioneerde full-stack web engineer heb ik mijn
+            vaardigheden verbeterd over een breed spectrum aan programmeertalen
+            en frameworks. Over de jaren heen heb ik gewerkt aan diverse
+            professionele en persoonlijke projecten die mijn vermogen
+            weerspiegelen om complexe problemen op te lossen en oplossingen van
+            hoge kwaliteit te leveren.
+          </p>
+
+          <p>
+            Ontdek de onderstaande projecten om een beter idee te krijgen van
+            hoe ik mijn expertise inzet voor praktische uitdagingen.
+          </p>
+        </div>
+
+        {projects?.map((project) => (
+          <ImageField
+            img={{
+              src: `/img/projects/${project.image}`,
+              alt: '',
+            }}
+            key={project.title}
+            label={project.title}
+            link={`/nl/projects/${project.path}`}
+            subtext=""
+            text=""
+          />
+        ))}
       </ContentContainer>
     </div>
   )
