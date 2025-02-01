2ce98f29b33dd3d9aa3cc899947f19ca102bd18c
Connor van Spronssen <info@dobefu.com>
2025-02-01 13:58:19 +0100

    fix: Resolve a caching issue

--- src/app/nl/projects/(project)/layout.tsx
+++ src/app/nl/projects/(project)/layout.tsx
@@ -1,15 +1,15 @@
+'use client'
+
 import ProjectLayout from '@/components/layout/project-layout'
 import getProjectFromUrl from '@/utils/get-project-from-url'
-import { headers } from 'next/headers'
-import { notFound } from 'next/navigation'
+import { notFound, usePathname } from 'next/navigation'
 
 export type Props = Readonly<{
   children: React.ReactNode
 }>
 
-export default async function Layout({ children }: Props) {
-  const headersList = await headers()
-  const url = headersList.get('x-url') ?? ''
+export default function Layout({ children }: Props) {
+  const url = usePathname()
   const project = getProjectFromUrl(url, 'nl')
   if (!project) notFound()
 
