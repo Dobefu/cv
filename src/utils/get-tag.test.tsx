1be0c9de12694be8579a08947c2d588b12e8ed23
Connor van Spronssen <info@dobefu.com>
2025-02-01 12:09:45 +0100

    test: Add tests to the getTag function

---
+++ src/utils/get-tag.test.tsx
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest'
+import getTag from './get-tag'
+
+describe('get-tag', () => {
+  it('Returns a valid tag', () => {
+    const tag = getTag('go')
+
+    expect(tag.title).toEqual('Go')
+  })
+
+  it("Returns a fallback tag when the tag doesn't exist", () => {
+    const tag = getTag('bogus')
+
+    expect(tag).toStrictEqual({
+      title: 'Unknown tag',
+      icon: 'mdi:question-mark-rhombus',
+      classes: '',
+    })
+  })
+})
