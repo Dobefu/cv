1f1e1fb91f9a6bb52878a31dfdf024b679d2230a
Connor van Spronssen <info@dobefu.com>
2025-02-02 15:51:35 +0100

    fix: Add a resolution for the maskable icon in the manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,7 +4,6 @@ import getConfig from 'next/config'
 export default function manifest(): MetadataRoute.Manifest {
   const { publicRuntimeConfig } = getConfig()
   const appName = publicRuntimeConfig.appName
-  const type = 'image/png'
 
   return {
     name: appName,
@@ -18,52 +17,43 @@ export default function manifest(): MetadataRoute.Manifest {
       {
         src: '/logo-maskable.png',
         purpose: 'maskable',
-        type,
+        sizes: '512x512',
       },
       {
         src: '/logo-72.png',
         sizes: '72x72',
-        type,
       },
       {
         src: '/logo-96.png',
         sizes: '96x96',
-        type,
       },
       {
         src: '/logo-128.png',
         sizes: '128x128',
-        type,
       },
       {
         src: '/logo-144.png',
         sizes: '144x144',
-        type,
       },
       {
         src: '/logo-152.png',
         sizes: '152x152',
-        type,
       },
       {
         src: '/logo-192.png',
         sizes: '192x192',
-        type,
       },
       {
         src: '/logo-256.png',
         sizes: '256x256',
-        type,
       },
       {
         src: '/logo-384.png',
         sizes: '384x384',
-        type,
       },
       {
         src: '/logo-512.png',
         sizes: '512x512',
-        type,
       },
     ],
   }
