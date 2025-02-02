1c535d5ccbe1ad45a697790c335f35fe39195723
Connor van Spronssen <info@dobefu.com>
2025-02-02 18:12:40 +0100

    fix: Remove redundant spacing on 404 pages

--- src/app/en/not-found.tsx
+++ src/app/en/not-found.tsx
@@ -2,12 +2,10 @@ import ContentContainer from '@/components/layout/content-container'
 
 export default function NotFound() {
   return (
-    <div className="relative px-4">
-      <ContentContainer>
-        <h2>Page not found</h2>
+    <ContentContainer>
+      <h2>Page not found</h2>
 
-        <p>The requested page could not be found.</p>
-      </ContentContainer>
-    </div>
+      <p>The requested page could not be found.</p>
+    </ContentContainer>
   )
 }
--- src/app/nl/not-found.tsx
+++ src/app/nl/not-found.tsx
@@ -2,12 +2,10 @@ import ContentContainer from '@/components/layout/content-container'
 
 export default function NotFound() {
   return (
-    <div className="relative px-4">
-      <ContentContainer>
-        <h2>Pagina niet gevonden</h2>
+    <ContentContainer>
+      <h2>Pagina niet gevonden</h2>
 
-        <p>De opgevraagde pagina kon niet worden gevonden.</p>
-      </ContentContainer>
-    </div>
+      <p>De opgevraagde pagina kon niet worden gevonden.</p>
+    </ContentContainer>
   )
 }
