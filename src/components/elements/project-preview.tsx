355911a756da13ef9e851e98261657dbb90bd0ca
Connor van Spronssen <info@dobefu.com>
2025-02-02 11:29:06 +0100

    fix: Prevent project preview images from shrinking

--- src/components/elements/project-preview.tsx
+++ src/components/elements/project-preview.tsx
@@ -22,7 +22,7 @@ export default function ProjectPreview({
       className="group my-4 inline-flex w-full items-center gap-4 border-t border-zinc-300 pt-12 max-md:min-w-full max-md:flex-col max-md:text-center md:gap-8 dark:border-zinc-600"
       href={link}
     >
-      <div className="overflow-hidden rounded-md drop-shadow-md">
+      <div className="shrink-0 overflow-hidden rounded-md drop-shadow-md">
         <Image
           alt=""
           className="transition-transform duration-300 group-hover:scale-105"
