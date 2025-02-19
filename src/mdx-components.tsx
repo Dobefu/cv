403b2e3c1bfc44e3e10f63d9b5b08d8575fb3cfd
Connor van Spronssen <info@dobefu.com>
2025-02-19 19:34:22 +0100

    feat: Make the Markdown anchor tag a locale link

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,16 @@
 import type { MDXComponents } from 'mdx/types'
+import LocaleLink from './components/utils/locale-link.client'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    a: ({ children, href }) => (
+      <LocaleLink
+        className="text-sky-700 hover:underline dark:text-sky-500"
+        href={href}
+      >
+        {children}
+      </LocaleLink>
+    ),
   }
 }
