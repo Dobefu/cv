8fdd796b63508eb04c2da1b295681823a258a714
Connor van Spronssen <info@dobefu.com>
2025-02-23 12:23:46 +0100

    chore: Simplify the HTML structure of the hero component

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -24,11 +24,11 @@ type Props = Readonly<{
 
 export default function Hero({ title, subtitle, img, imgBg, cta }: Props) {
   return (
-    <section className="relative -mt-28 -mb-8 h-[30vh] min-h-[450px] w-full">
+    <section className="relative -mt-28 -mb-8 h-[30vh] min-h-[450px]">
       {!!imgBg && (
         <Image
           alt={imgBg.alt}
-          className="absolute inset-0 h-[30vh] min-h-[450px] w-screen object-cover"
+          className="min-h-[450px] object-cover"
           fill
           loading="eager"
           priority
@@ -38,43 +38,43 @@ export default function Hero({ title, subtitle, img, imgBg, cta }: Props) {
         />
       )}
 
-      <div className="absolute top-0 h-full w-full bg-black/20 p-4 max-sm:p-2">
-        <div className="mx-auto h-full max-w-5xl p-8">
-          <div className="flex h-full text-white">
-            <div className="mt-8 flex h-full flex-col justify-center gap-2">
-              <h1 className="text-5xl font-medium text-white drop-shadow-md max-md:text-4xl max-sm:text-3xl">
-                {title}
-              </h1>
-              <h2 className="text-4xl text-white drop-shadow-md max-md:text-2xl max-sm:text-xl">
-                {subtitle}
-              </h2>
-              {cta ? (
-                <LocaleLink
-                  className="me-auto flex items-center gap-2 rounded-lg border border-transparent bg-sky-700 px-3 py-2 text-sm font-medium text-white transition-colors hover:bg-sky-800 focus:bg-sky-800 focus:outline-hidden sm:px-4 sm:py-3 print:hidden"
-                  href={cta.href}
-                >
-                  <Icon className="size-5" icon={iconChevronRight} ssr />
+      <div className="absolute h-full w-full bg-black/20 p-4 max-sm:p-2">
+        <div className="mx-auto flex h-full max-w-5xl p-8">
+          <div className="mt-8 flex h-full flex-col justify-center gap-2">
+            <h1 className="text-5xl text-white drop-shadow-md max-sm:text-3xl">
+              {title}
+            </h1>
 
-                  {cta.title}
-                </LocaleLink>
-              ) : undefined}
-            </div>
+            <h2 className="text-4xl text-white drop-shadow-md max-sm:text-xl">
+              {subtitle}
+            </h2>
 
-            {!!img && (
-              <Image
-                alt={img.alt}
-                className="relative mx-auto mt-auto -mb-4 w-64 flex-none origin-bottom drop-shadow-md transition-transform active:scale-95 max-sm:-ms-8 max-sm:-mb-2 max-sm:w-32"
-                height={256}
-                loading="eager"
-                priority
-                quality={70}
-                sizes="(max-width: 640px) 128px, (max-width: 768px) 256px, (max-width: 1024px) 512px"
-                src={img.src}
-                title={`${title} - ${subtitle}`}
-                width={256}
-              />
-            )}
+            {cta ? (
+              <LocaleLink
+                className="me-auto flex gap-2 rounded-lg bg-sky-700 px-3 py-2 text-sm font-medium text-white transition-colors hover:bg-sky-800 focus:bg-sky-800 sm:px-4 sm:py-3 print:hidden"
+                href={cta.href}
+              >
+                <Icon className="size-5" icon={iconChevronRight} ssr />
+
+                {cta.title}
+              </LocaleLink>
+            ) : undefined}
           </div>
+
+          {!!img && (
+            <Image
+              alt={img.alt}
+              className="relative mx-auto mt-auto -mb-4 w-64 flex-none origin-bottom drop-shadow-md transition-transform active:scale-95 max-sm:-ms-8 max-sm:-mb-2 max-sm:w-32"
+              height={256}
+              loading="eager"
+              priority
+              quality={70}
+              sizes="(max-width: 640px) 128px, (max-width: 768px) 256px, (max-width: 1024px) 512px"
+              src={img.src}
+              title={`${title} - ${subtitle}`}
+              width={256}
+            />
+          )}
         </div>
       </div>
     </section>
