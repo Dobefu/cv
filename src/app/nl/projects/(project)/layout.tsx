5cafdf59037f0d3f21149b0fd735087683418643
Connor van Spronssen <info@dobefu.com>
2025-02-28 12:02:51 +0100

    fix: Make the project layout a client component again

--- src/app/nl/projects/(project)/layout.tsx
+++ src/app/nl/projects/(project)/layout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import ProjectLayout from '@/components/layout/project-layout'
 import getProjectFromUrl from '@/utils/get-project-from-url'
 import { notFound, usePathname } from 'next/navigation'
