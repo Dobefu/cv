200c704aa45388cb3b2faf378cc23919b7166add
Connor van Spronssen <info@dobefu.com>
2025-03-01 19:13:26 +0100

    refactor: Move the skip to main HTML classes to the CSS

--- src/components/utils/skip-to-main.tsx
+++ src/components/utils/skip-to-main.tsx
@@ -6,11 +6,7 @@ export type Props = Readonly<{
 
 export default function SkipToMain({ locale }: Props) {
   return (
-    <a
-      className="fixed z-50 bg-white px-4 py-2 not-focus:-translate-y-full not-focus:opacity-0 dark:bg-zinc-800"
-      href="#main"
-      tabIndex={0}
-    >
+    <a className="skip-to-main" href="#main" tabIndex={0}>
       {getTranslation(locale, 'skip_to_main.title')}
     </a>
   )
