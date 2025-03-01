0c5d685e79f4a2181a61f253a667ce1135419041
Connor van Spronssen <info@dobefu.com>
2025-03-01 15:56:40 +0100

    refactor: Move the hero component to the client

--- src/components/layout/hero.client.test.tsx
+++ src/components/layout/hero.client.test.tsx
@@ -1,6 +1,6 @@
 import { cleanup, render, screen } from '@testing-library/react'
 import { afterEach, describe, expect, it, vi } from 'vitest'
-import Hero from './hero'
+import Hero from './hero.client'
 
 describe('Hero', () => {
   afterEach(() => {
--- src/components/layout/hero.client.tsx
+++ src/components/layout/hero.client.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import iconChevronRight from '@iconify/icons-mdi/chevron-right'
 import { Icon } from '@iconify/react'
 import Image from 'next/image'
